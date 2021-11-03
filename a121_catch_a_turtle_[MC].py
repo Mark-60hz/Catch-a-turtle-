fb335ebc76172fb29c3a9a0e13fc5fa3a9877d83
Mark-60hz <91716369+Mark-60hz@users.noreply.github.com>
2021-11-03 13:06:43 -0700

    Update a121_catch_a_turtle_[MC].py

--- a121_catch_a_turtle_[MC].py
+++ a121_catch_a_turtle_[MC].py
@@ -2,30 +2,30 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
-
 #-----game configuration----
-spotColor = "red"
-spotShape = "square"
+spotColor = "pink"
+spotShape = "circle"
 spotSize = 5
 score = 0
-font_setup = ("Arial", 20, "normal")
-
 #-----initialize turtle-----
 spot = trtl.Turtle()
 spot.shape(spotShape)
 spot.turtlesize(spotSize)
 spot.fillcolor(spotColor)
-
+spot.speed(0)
 score_writer = trtl.Turtle()
 score_writer.pu()
 score_writer.goto(-180,170)
-
 spot.penup()
+#-------Background color--------
+wn=trtl.Screen()
+wn.bgcolor("grey")
+wn.title("This is my screen title!")
 #-----game functions--------
 def spot_clicked(x,y):
-  change_position()
+  size = rand.randint(1, 10)
+  change_position(size)
   update_score()
-
 def change_position(size):
   new_xpos = rand.randint(-180,180)
   new_ypos = rand.randint(-140,140)
@@ -34,23 +34,14 @@ def change_position(size):
   spot.hideturtle()
   spot.fillcolor(colors[colorcycle])
   spot.turtlesize(size)
-
-def change_position():
-  new_xpos = rand.randint(-180,180)
-  new_ypos = rand.randint(-140,140)
-  spot.hideturtle()
   spot.goto(new_xpos,new_ypos)
   spot.showturtle()
-
 def update_score():
   global score 
   score += 1
   print(score)
   score_writer.clear()
   score_writer.write(score, font=font_setup)
-
-#-----events----------------
-spot.onclick(spot_clicked)
 #-----events----------------
 spot.onclick(spot_clicked)
 font_setup = ("Arial", 20, "normal")
@@ -60,9 +51,13 @@ timer_up = False
 #-----countdown writer-----
 counter =  trtl.Turtle()
 #-----game functions-----
+counter.penup()
+counter.goto(200, 200)
+counter.pendown()
 def countdown():
   global timer, timer_up
   counter.clear()
+  
   if timer <= 0:
     counter.write("Time's Up", font=font_setup)
     timer_up = True
